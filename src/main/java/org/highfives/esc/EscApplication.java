c984b9bbd6d9c78ba431d3590437e08f70ff7b00
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-04-04 16:38:39 +0900

    First commit!

---
+++ src/main/java/org/highfives/esc/EscApplication.java
@@ -0,0 +1,13 @@
+package org.highfives.esc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscApplication.class, args);
+    }
+
+}
