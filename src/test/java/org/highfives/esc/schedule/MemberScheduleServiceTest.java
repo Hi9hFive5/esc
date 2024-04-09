707046aed72241cedd3fcba5d38a6f72687f9f17
oneye <onelee521@gmail.com>
2024-04-09 14:18:54 +0900

    feature(MemberSchedule): id로 멤버 일정 조회 서비스 구현 추가
    테스트파일이 누락되어 추가함

---
+++ src/test/java/org/highfives/esc/schedule/MemberScheduleServiceTest.java
@@ -0,0 +1,28 @@
+package org.highfives.esc.schedule.service;
+
+import org.highfives.esc.schedule.dto.MemberScheduleDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberScheduleServiceTest {
+
+    @Autowired
+    MemberScheduleService memberScheduleService;
+
+    @Test
+    @DisplayName("멤버일정 id로 멤버 일정 조회")
+    void findMemberScheduleByIdTest() {
+        int id = 1;
+
+        MemberScheduleDTO memberScheduleDTO = memberScheduleService.findMemberScheduleById(id);
+        System.out.println(memberScheduleDTO);
+
+        assertEquals(memberScheduleDTO.getId(), id);
+        assertEquals(memberScheduleDTO.getId(), id);
+    }
+}
