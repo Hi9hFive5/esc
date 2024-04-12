21ae6e0c004b8617576024d709ef65271e27b85d
oneye <onelee521@gmail.com>
2024-04-12 23:37:33 +0900

    feature(StudySchedule): 스터디일정 조회 기능 컨트롤러 구현
    - id로 조회
    - 스터디클럽 id로 조회

--- src/main/java/org/highfives/esc/schedule/controller/StudyScheduleController.java
+++ src/main/java/org/highfives/esc/schedule/controller/StudyScheduleController.java
@@ -1,10 +1,19 @@
 package org.highfives.esc.schedule.controller;
 
+import org.highfives.esc.schedule.dto.StudyScheduleDTO;
 import org.highfives.esc.schedule.service.StudyScheduleService;
+import org.highfives.esc.schedule.vo.ResponseStudyScheduleListVO;
+import org.highfives.esc.schedule.vo.ResponseStudyScheduleVO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/study-schedule")
@@ -14,9 +23,54 @@ public class StudyScheduleController {
     private final StudyScheduleService studyScheduleService;
 
     @Autowired
-
     public StudyScheduleController(ModelMapper mapper, StudyScheduleService studyScheduleService) {
         this.mapper = mapper;
         this.studyScheduleService = studyScheduleService;
+    }
+
+    @GetMapping("/schdule/{id}")
+    public ResponseEntity<ResponseStudyScheduleVO> findStudyScheduleById(
+            @PathVariable("id") int id) {
+        ResponseStudyScheduleVO response = new ResponseStudyScheduleVO();
+
+        StudyScheduleDTO studyScheduleDTO = studyScheduleService.findStudyScheduleById(id);
+        if(studyScheduleDTO != null){
+
+
+            response = mapper.map(studyScheduleDTO, ResponseStudyScheduleVO.class);
+//            response.setId(studyScheduleDTO.getId());
+//            response.setTitle(studyScheduleDTO.getTitle());
+//            response.setContent(studyScheduleDTO.getContent());
+//            response.setStartDatetime(studyScheduleDTO.getStartDatetime());
+//            response.setEndDatetime(studyScheduleDTO.getEndDatetime());
+//            response.setUseState(studyScheduleDTO.getUseState());
+//            response.
+            response.setMessage("조회 성공");
+        } else {
+            response.setMessage("조회 실패");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/studyculb/{studyculbId}")
+    public ResponseEntity<ResponseStudyScheduleListVO> findStudyScheduleByStudyclubId(
+            @PathVariable("studyculbId") int studyculbId) {
+        ResponseStudyScheduleListVO response = new ResponseStudyScheduleListVO();
+
+        ArrayList<StudyScheduleDTO> studyScheduleDTOList = studyScheduleService.findStudyScheduleByStudyId(studyculbId);
+        ArrayList<ResponseStudyScheduleVO> studyScheduleVOList = new ArrayList<>();
+        if(studyScheduleDTOList != null){
+
+            for(StudyScheduleDTO studyScheduleDTO: studyScheduleDTOList){
+                studyScheduleVOList.add(mapper.map(studyScheduleDTO, ResponseStudyScheduleVO.class));
+            }
+
+            response.setMessage("조회 성공");
+            response.setStudySchedules(studyScheduleVOList);
+        } else {
+            response.setMessage("조회 실패");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
---
+++ src/main/java/org/highfives/esc/schedule/vo/ResponseStudyScheduleVO.java
@@ -0,0 +1,23 @@
+package org.highfives.esc.schedule.vo;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ResponseStudyScheduleVO {
+    private String message;                 // 성공여부 메세지
+
+    private int id;                         // 스터디 일정 id
+    private String title;                   // 일정 제목
+    private String content;                 // 일정 내용
+    private LocalDateTime startDatetime;    // 일정 시작 시간
+    private LocalDateTime endDatetime;      // 일정 종료 시간
+    private char useState;                  // 일정 삭제 여부
+    private int writerId;                   // 일정 작성 멤버(리더)
+    private int studyclubId;                // 해당 스터디클럽
+}
