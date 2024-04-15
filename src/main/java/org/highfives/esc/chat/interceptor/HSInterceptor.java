9439dfaca86d76de05860324b6b656dca007acb0
t4e1 <jtw5738@naver.com>
2024-04-15 15:49:33 +0900

    Feat: 채팅방 접속 인원수 추가 기능 구현

--- src/main/java/org/highfives/esc/chat/interceptor/HSInterceptor.java
+++ src/main/java/org/highfives/esc/chat/interceptor/HSInterceptor.java
@@ -1,7 +1,9 @@
 package org.highfives.esc.chat.interceptor;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -30,13 +32,13 @@ public class HSInterceptor implements HandshakeInterceptor {
                                    Map<String, Object> attributes) throws Exception {
         String path = request.getURI().getPath();
         String roomId = path.substring(path.lastIndexOf('/') + 1);
-
+        System.out.println("request.getHeaders().getOrigin() = " + request.getHeaders().getOrigin());
         /* http://~/chat/{roomId} 에서 roomId부분을 따로 떼서 Map으로 저장*/
         attributes.put("roomId", roomId);
-
         System.out.println("uri = " + request.getURI());
         System.out.println("path = " + path);
         System.out.println("roomId = " + roomId);
+
         return true;
     }
 
@@ -45,6 +47,14 @@ public class HSInterceptor implements HandshakeInterceptor {
                                ServerHttpResponse response,
                                WebSocketHandler wsHandler,
                                Exception exception) {
-
+        if (request instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpSession session = servletRequest.getServletRequest().getSession(false);
+            if (session != null) {
+                String sessionId = session.getId();
+                // 여기에서 세션 ID를 사용할 수 있습니다.
+                System.out.println("Session ID: " + sessionId);
+            }
+        }
     }
 }
